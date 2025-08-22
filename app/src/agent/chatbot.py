2fcb5f33ef4faabeffda90fa97c05256eb4c007c
Bruno Chiconato <brunochiconato01@gmail.com>
2025-08-22 15:49:51 -0300

    feat(chatbot): add example of chatbot with summarization
    This commit introduces the script from module 2, lesson 6, which demonstrates how to build a stateful chatbot with conversation memory summarization.
    
    - Implements a graph that uses a custom state to store a running summary.
    - A conditional edge checks the conversation length.
    - If the conversation exceeds a threshold, a dedicated node generates a summary of the history and prunes older messages to manage the context window.

---
+++ app/src/agent/chatbot.py
@@ -0,0 +1,70 @@
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, RemoveMessage
+from langgraph.graph import StateGraph, MessagesState, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2")
+
+
+class State(MessagesState):
+    summary: str
+
+
+model = ChatOpenAI(model="gpt-4o", temperature=0)
+
+
+def call_model(state: State):
+    summary = state.get("summary", "")
+
+    if summary:
+        system_message = f"Summary of conversation earlier: {summary}"
+        messages = [SystemMessage(content=system_message)] + state["messages"]
+    else:
+        messages = state["messages"]
+
+    response = model.invoke(messages)
+
+    return {"messages": response}
+
+
+def summarize_conversation(state: State):
+    summary = state.get("summary", "")
+
+    if summary:
+        summary_message = (
+            f"This is summary of the conversation to date: {summary}\n\n"
+            "Extend the summary by taking into account the new messages above:"
+        )
+    else:
+        summary_message = "Create a summary of the conversation above:"
+
+    messages = state["messages"] + [HumanMessage(content=summary_message)]
+    response = model.invoke(messages)
+
+    delete_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-2]]
+
+    return {"summary": response.content, "messages": delete_messages}
+
+def should_continue(state: State):
+    """Return the next node to execute."""
+    messages = state["messages"]
+
+    if len(messages) > 6:
+        return "summarize_conversation"
+    
+    return END
+
+workflow = StateGraph(State)
+workflow.add_node("conversation", call_model)
+workflow.add_node(summarize_conversation)
+
+workflow.add_edge(START, "conversation")
+workflow.add_conditional_edges("conversation", should_continue)
+workflow.add_edge("summarize_conversation", END)
+
+chatbot = workflow.compile()
